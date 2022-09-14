6cb9f7c2de1614c79f0ff7d44bc65ee24e4850e1
euviniciuss <dev.mvsc@gmail.com>
2022-09-14 15:03:56 -0300

    feat: criando configuração do expo com Hello World

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+import { styles } from './styles'
+
+export default function Home() {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.message}>Hello World</Text>
+    </View>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  message: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
+})
